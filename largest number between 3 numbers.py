d3728b035b0a492766fe0de731b3a12962a7f124
Parth Padmawar <parthpadmawar4@gmail.com>
2024-12-16 22:46:34 +0530

    By mistake

--- largest number between 3 numbers.py
+++
@@ -1,9 +0,0 @@
-A= input("Enter number A: ")
-B= input("Enter number B: ")
-C= input("Enter number C: ")
-if A > B and A > C:
-    print("A is the largest number!")
-elif B > A and B > C:
-    print("B is the largest number!")  
-elif C > A and C > B:
-    print("C is the largest number!")  
