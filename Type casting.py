402f831529d4a89d0fb799d191193d0fb71dfaac
Parth Padmawar <parthpadmawar4@gmail.com>
2024-12-19 18:38:51 +0530

    Type casting

---
+++ Type casting.py
@@ -0,0 +1,20 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool() 
+
+name = "Parth"
+age = 19
+gpa = 9.3
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+age = float(age)     #changing the int to float
+gpa = int(gpa)       #changing the float to int
+name = bool(name)    #if we have written someting then it willl get tru eo rit will show false "that how we can get some on to give input" 
+
+print(age) 
+print(gpa)
+print(name)
