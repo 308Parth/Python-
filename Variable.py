6f83e4f742e1dec97eb2e704cf6fac2aaca15292
Parth Padmawar <parthpadmawar4@gmail.com>
2024-12-19 18:26:13 +0530

    Variable

---
+++ Variable.py
@@ -0,0 +1,26 @@
+#Variable = A container for a value (string, integer, float, boolean)
+#           A variable behaves as if it was the value it contains
+
+#String
+first_name = "Parth"
+food = "Pizza"
+
+#Integers
+age = 19
+quqntity = 3 
+num_of_students = 10
+
+#Float
+price = 10.99
+gpa = 9.8
+distance = 56.5
+
+# Boolean
+is_student = False
+for_sale = False
+is_online = True
+
+if is_online:
+    print("That item is for sale")
+else:
+    print("That item is NOT availabe")
