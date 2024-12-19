2f01720d07c080aa792f775937b50b88956e3a8d
Parth Padmawar <parthpadmawar4@gmail.com>
2024-12-19 19:05:59 +0530

    Arithmatic and math oprator

---
+++ Arithmatic and math oprator.py
@@ -0,0 +1,24 @@
+#x = 3.14
+#y = 4
+#z = 5
+
+#result = round(x)  #gives roundoff value
+#result = abs(y)    #gives absolute value
+#result = pow(4, 3)  #4 to the power 3 
+#result = max(x, y, z)  # tells which value is the greatest 
+
+#print(result)
+
+#---------------------------------------------------------------------
+
+import math
+
+x = 9.9999
+
+print(math.pi) # vslue of pi
+print(math.e) # value of e
+#result = math.sqrt(x)  # square root
+#result = math.ceil(x)   # roundoff to the greatest integer frim the pair in which it stand
+result = math.floor(x)   # roundsoff to the smallest on of the pair 
+
+print(result)
